1394455e6d6b195a669f910ab42aafccfd14c82b
RobotEby <ubuntu.onion@hotmail.com>
2025-09-30 15:07:05 -0300

    feat: set the dispatch type

--- dog-api/src/types/types.ts
+++ dog-api/src/types/types.ts
@@ -1,5 +1,9 @@
+import type { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
+
 export type ReduxState = {
   isFetching: boolean;
   imageURL: string;
   errorMessage: string;
 };
+
+export type Dispatch = ThunkDispatch<ReduxState, null, AnyAction>;
