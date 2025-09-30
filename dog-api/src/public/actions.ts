6396a6a4bfb96401285e64557803ed393be3bdc0
RobotEby <ubuntu.onion@hotmail.com>
2025-09-30 15:24:46 -0300

    feat: creating the thunk action creator

--- dog-api/src/public/actions.ts
+++ dog-api/src/public/actions.ts
@@ -1,4 +1,10 @@
-function requestStarte() {
+export const REQUEST_STARTED = 'REQUEST_STARTED';
+export const REQUEST_SUCCESSFUL = 'REQUEST_SUCCESSFUL';
+export const REQUEST_FAILED = 'REQUEST_FAILED';
+
+import { Dispatch } from '../types/types';
+
+function requestStarted() {
   return { type: REQUEST_STARTED };
 }
 
@@ -13,5 +19,18 @@ function requestFailed(error: string) {
   return {
     type: REQUEST_FAILED,
     payload: error,
+  };
+}
+
+export function fetchDogImage() {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch(requestStarted());
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      dispatch(requestSuccessful(data));
+    } catch (error: any) {
+      dispatch(requestFailed(error.message));
+    }
   };
 }
