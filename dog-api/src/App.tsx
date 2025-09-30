8747b702cf066479876b9bd23e53ee3f03e5bf9f
RobotEby <ubuntu.onion@hotmail.com>
2025-09-30 16:35:13 -0300

    feat: render the image

--- dog-api/src/App.tsx
+++ dog-api/src/App.tsx
@@ -1,16 +1,25 @@
-import { useDispatch, UseDispatch, useSelector } from 'react-redux';
-import { Dispatch, ReduxState } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
+import type { ThunkDispatch, ThunkAction } from 'redux-thunk';
+import type { AnyAction } from 'redux';
 import { fetchDogImage } from './public/actions';
 
+interface RootState {
+  isFetching: boolean;
+  imageURL: string;
+  errorMessage: string;
+}
+
 function App() {
-  const rootState = useSelector((state: ReduxState) => state);
-  const dispatch: Dispatch = useDispatch();
+  const rootState = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<ThunkDispatch<RootState, unknown, AnyAction>>();
+
+  if (rootState.isFetching) return <p>Loading...</p>;
 
   return (
     <div>
       <button
         onClick={() => {
-          dispatch(fetchDogImage());
+          dispatch(fetchDogImage() as ThunkAction<Promise<void>, RootState, unknown, AnyAction>);
         }}
       >
         New dog
